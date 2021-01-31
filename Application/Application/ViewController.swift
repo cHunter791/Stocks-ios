9d278bdb44a86bb66a8a2423479075adc756d37d
Christoper Hunter <chunter791@gmail.com>
2021-01-31 16:22:34 +0000

    Initial commit

---
+++ Application/Application/ViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+import Core
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        sayHiTo(name: "Chris")
+    }
+}
---
+++ Core/Core/TestApi.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+public func sayHiTo(name value: String) {
+    print(value)
+}
